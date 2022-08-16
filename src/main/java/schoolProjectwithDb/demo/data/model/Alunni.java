cfb187ec38cc9bf4d9facffb4c88d3a2458fee01
Nicola Vitale <nico.vits@gmail.com>
2022-08-16 16:57:02 +0200

    Creata classe Alunni in package model

---
+++ src/main/java/schoolProjectwithDb/demo/data/model/Alunni.java
@@ -0,0 +1,34 @@
+package schoolProjectwithDb.demo.data.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "alunni")
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alunni {
+
+  @Id
+  @Column
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  @Column
+  private String nome;
+  @Column
+  private String cognome;
+  @Column
+  private Date data_nascita;
+
+}
